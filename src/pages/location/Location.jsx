01e19b757cc4e294e56092d16270c9d3399034d7
Pedro Gabriel <pedrogabriellima@gmail.com>
2024-02-18 20:47:00 -0300

    type

--- src/pages/location/Location.jsx
+++ src/pages/location/Location.jsx
@@ -26,7 +26,7 @@ export default function Location() {
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.9266606295687!2d-34.87699712545589!3d-7.134479669976251!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ace810918e1e95%3A0xd03c0c2a69698de2!2sRua%20Carmelo%20Ruffo%2C%20197%20-%20Jaguaribe%2C%20Jo%C3%A3o%20Pessoa%20-%20PB%2C%2058015-460!5e0!3m2!1spt-BR!2sbr!4v1708288239788!5m2!1spt-BR!2sbr" width="600px" height="450" loading="lazy" title='Localização do salão Bendita Beleza.'>
                 </iframe>
                 <div className='iframe-data'>
-                    <p className="iframe-paragraph">Rua Carmelo Ruffo, 197 - Jaguaribe, João Pessoa - Paraíba.</p>
+                    <p className="iframe-paragraph">Rua Carmelo Ruffo, 197, Jaguaribe, João Pessoa - Paraíba.</p>
                     <p className="iframe-paragraph">Localizado ao lado do Instituto Federal de Educação da Paraíba - IFPB.</p>
                     <p className="iframe-paragraph">Horário de funcionamento: <span>Segunda a Sábado, das 09:00 às 18:00.</span></p>
                 </div>
