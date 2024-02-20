1453b3d9cea0e56b5fc9b7f747a3496ef68dcfd3
Pedro Gabriel <pedrogabriellima@gmail.com>
2024-02-20 19:17:02 -0300

    svg to text and button size of carousel card

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,15 +2,14 @@ import { home_images } from "../../utils/images";
 
 import { CarouselCard } from "../../components/cards/carouselcard/CarouselCard";
 
-import BenditaBelezaCabeleireira from "../../assets/logo/benditabelezacabeleireira.svg";
-
 import "./home.scss";
 
 export default function Home() {
     return (
         <main id="home-page">
             <CarouselCard images={home_images}/>
-            <img className="bendita-beleza-cabeleireira-logo" src={BenditaBelezaCabeleireira} alt="" />
+            <h2 className="bendita-beleza-title">Bendita Beleza</h2>
+            <p className="bendita-beleza-paragraph">Cabeleireira</p>
         </main>
     )
 }
