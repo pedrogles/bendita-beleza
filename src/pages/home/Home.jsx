49603b917c6b36d9181a4b53ddb9101b66175e7d
Pedro Gabriel <pedrogabriellima@gmail.com>
2024-10-15 14:09:43 -0300

    Refactor: break down home  page into smaller components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,13 @@
-import { home_images } from "../../utils/images";
-
-import { CarouselCard } from "../../components/cards/carouselcard/CarouselCard";
+import { CarouselCard } from "./components";
+import { BenditaBelezaLogo } from "../../components/benditaBelezaLogo/BenditaBelezaLogo";
 
 import "./home.scss";
 
 export default function Home() {
     return (
         <main id="home-page">
-            <CarouselCard images={home_images}/>
-            <h2 className="bendita-beleza-title">Bendita Beleza</h2>
-            <p className="bendita-beleza-paragraph">Cabeleireira</p>
+            <CarouselCard/>
+            <BenditaBelezaLogo image={false} />
         </main>
-    )
-}
+    );
+};
---
+++ src/pages/home/components/index.js
@@ -0,0 +1,1 @@
+export { CarouselCard } from './carouselcard/CarouselCard';
